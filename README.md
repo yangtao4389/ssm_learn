c080e85af4dd60a795d260a486fb52524923e685
yangtao <438936731@qq.com>
2019-11-29 12:22:06 +0800

    first version

---
+++ README.md
@@ -0,0 +1,3 @@
+### 整合SSM框架：SpringMVC + Spring + MyBatis
+参考链接：  
+https://blog.csdn.net/qq598535550/article/details/51703190
---
+++ src/test/java/service/impl/BookServiceImplTest.java
@@ -0,0 +1,22 @@
+package service.impl;
+
+import base.BaseTest;
+import dto.AppointExecution;
+import service.BookService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookServiceImplTest extends BaseTest {
+
+	@Autowired
+	private BookService bookService;
+
+	@Test
+	public void testAppoint() throws Exception {
+		long bookId = 1001;
+		long studentId = 12345678910L;
+		AppointExecution execution = bookService.appoint(bookId, studentId);
+		System.out.println(execution);
+	}
+
+}
